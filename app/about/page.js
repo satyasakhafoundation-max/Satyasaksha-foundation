0082e1b7b6ed13d8d446682915fed5d49191e50d
unknown <aditya.kokate47@gmail.com>
2026-09-15 17:49:47 +0530

    Hide leadership section

--- app/about/page.js
+++ app/about/page.js
@@ -78,7 +78,7 @@ export default function AboutPage() {
         </div>
       </section>
 
-      {/* Team Section */}
+      {/* Team Section (Hidden for now)
       <section className={`section ${styles.teamSection}`}>
         <div className="container">
           <div className="section-header reveal">
@@ -102,6 +102,7 @@ export default function AboutPage() {
           </div>
         </div>
       </section>
+      */}
 
     </div>
   );
