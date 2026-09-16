882a69accd7b2f92b83bbd5b9c410ccd9d02cc9d
unknown <aditya.kokate47@gmail.com>
2026-09-16 11:07:32 +0530

    Make the leadership (team) section visible again

--- app/about/page.js
+++ app/about/page.js
@@ -78,7 +78,7 @@ export default function AboutPage() {
         </div>
       </section>
 
-      {/* Team Section (Hidden for now)
+      {/* Team Section */}
       <section className={`section ${styles.teamSection}`}>
         <div className="container">
           <div className="section-header reveal">
@@ -102,7 +102,6 @@ export default function AboutPage() {
           </div>
         </div>
       </section>
-      */}
 
     </div>
   );
